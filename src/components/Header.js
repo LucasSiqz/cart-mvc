6972b4fd710b441387cbe3baa40c1c7fc6729674
Lucas Siqueira <lucassiqzro@gmail.com>
2023-03-31 12:39:58 -0300

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,9 +20,9 @@ headerComponent.innerHTML = `
     <h1 class="header__title">CartJS</h1>
     <div class="header__cart">
       <strong class="header__cart-title">Meu carrinho</strong>
-      <slot id="cart-quantity" name="cart-quantity"> 0 </slot>
+      <slot id="cart-quantity" name="cart-quantity"></slot>
     </div>
-</header>
+  </header>
 `
 
 class Header extends HTMLElement {
